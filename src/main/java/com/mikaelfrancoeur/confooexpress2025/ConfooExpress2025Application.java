bedc7a7d501a836edae3975ab7c9a749c3c6ef83
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-01-13 16:30:17 -0500

    initial commit

---
+++ src/main/java/com/mikaelfrancoeur/confooexpress2025/ConfooExpress2025Application.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.confooexpress2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfooExpress2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfooExpress2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikaelfrancoeur/confooexpress2025/ConfooExpress2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.confooexpress2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfooExpress2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
