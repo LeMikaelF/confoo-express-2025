1a0d3d761d768155f2ffd7caf7f6902d68fff4aa
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-01-14 19:48:55 -0500

    use cglib proxy

--- src/main/java/com/mikaelfrancoeur/confooexpress2025/person/LoudBeanPostProcessor.java
+++ src/main/java/com/mikaelfrancoeur/confooexpress2025/person/LoudBeanPostProcessor.java
@@ -2,6 +2,8 @@ package com.mikaelfrancoeur.confooexpress2025.person;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,14 @@ public class LoudBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
         if ("loudPerson".equals(beanName) && bean instanceof Person person) {
-            return new LoudPerson(person);
+            return Enhancer.create(Person.class,
+                    (MethodInterceptor) (obj, method, args, proxy) -> {
+                        Object result = method.invoke(person, args);
+                        if (method.getName().equals("greet") && result instanceof String string) {
+                            return (string).toUpperCase();
+                        }
+                        return result;
+                    });
         }
         return bean;
     }
